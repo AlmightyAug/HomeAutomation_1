c2eb66e6635bccdd05dc34c74c5c0e5f338fe056
AlmightyAug <lkyaugustine@gmail.com>
2017-01-21 14:20:06 +0800

    Trying out new optimisations

---
+++ app/src/main/java/fyp/ntu/scse/homeautomation/controller/Bluetooth_Program_Adapter.java
@@ -0,0 +1,15 @@
+package fyp.ntu.scse.homeautomation.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import.android.view.ViewGroup;
+import.android.widget.BaseAdapter;
+import.android.widget.ProgressBar;
+import.android.widget.TextView
+
+public class Bluetooth_Program_Adapter {
+
+}
