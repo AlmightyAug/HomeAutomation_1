04c9d971229adb6fa42a9482c9579a5dcde88524
AlmightyAug <lkyaugustine@gmail.com>
2017-01-27 12:04:48 +0800

    Trying out new optimisations

--- app/src/main/java/fyp/ntu/scse/homeautomation/controller/Bluetooth_Program_Adapter.java
+++ app/src/main/java/fyp/ntu/scse/homeautomation/controller/Bluetooth_Program_Adapter.java
@@ -5,11 +5,14 @@ import java.util.List;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.BaseAdapter;
 import.android.view.ViewGroup;
 import.android.widget.BaseAdapter;
 import.android.widget.ProgressBar;
 import.android.widget.TextView
 
-public class Bluetooth_Program_Adapter {
+public class Bluetooth_Program_Adapter extends BaseAdapter{
 
+    private List<programInfo> mList;
+    private final LayoutInflater mLayourInflater
 }
